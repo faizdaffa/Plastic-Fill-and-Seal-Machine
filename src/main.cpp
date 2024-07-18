5d674acd15450323a54d9f1c154f750c8e04dd55
faizdaffa <faizdafa15@gmail.com>
2024-07-18 14:59:51 +0700

    last update
    Tinggal heater vertical

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,7 @@
 unsigned long previousTime = 0;
 int stepsPerRotation = 200;
 int increaseStep = 500;
+int count = 0;
 
 void setup()
 {
@@ -17,79 +18,37 @@ void setup()
   accelStepper_innit();
   heater_innit();
   servo_open_position();
+  lcd.clear();
 }
 
-void manual_code()
+void manual_code() 
 {
-  // read temperature
+  // ===================================READ TEMPERATURE THERMOCOUPLE====================================
   temperature_V = thermocoupleVertical.readCelsius();
   temperature_H = thermocoupleHorizontal.readCelsius();
-  // servo open position
-  servoVertical.write(120);
-  servoHorizontal.write(65);
-  // stepper motor ON
-  stepperVertical.move(200);
-  stepperHorizontal.move(200);
-  while (stepperVertical.run() && stepperHorizontal.run())
-  {
-  }
-  // pompa OFF
-  digitalWrite(POMPA, HIGH);
-  // LCD Display
-  lcd.clear();
   lcd.setCursor(0, 0);
   lcd.print("Fill & Seal Machine");
   lcd.setCursor(0, 1);
   lcd.print("T_Verical   :" + String(temperature_V));
   lcd.setCursor(0, 2);
   lcd.print("T_Horizontal:" + String(temperature_H));
-  lcd.setCursor(0, 3);
-  lcd.print("Pull Plastic");
 
-  delay(1000);
-
-  //==============seal condition==============
-  // read temperature
-  temperature_V = thermocoupleVertical.readCelsius();
-  temperature_H = thermocoupleHorizontal.readCelsius();
-  // servo close position
-  servoVertical.write(20);
-  servoHorizontal.write(0);
-  // stepper motor OFF
-  // pompa ON
-  digitalWrite(POMPA, LOW);
-  // LCD Display
-  lcd.clear();
-  lcd.setCursor(0, 0);
-  lcd.print("Fill & Seal Machine");
-  lcd.setCursor(0, 1);
-  lcd.print("T_Verical   :" + String(temperature_V));
-  lcd.setCursor(0, 2);
-  lcd.print("T_Horizontal:" + String(temperature_H));
-  lcd.setCursor(0, 3);
-  lcd.print("FIll and Seal Plastic");
-
-  delay(2000);
-}
-
-void manual2() 
-{
-  temperature_V = thermocoupleVertical.readCelsius();
-  temperature_H = thermocoupleHorizontal.readCelsius();
-  // pompa OFF
+  // ==============================================Kondisi 1=============================================
+  // Kondisi 1: Pompa ON, Servo kebuka, Stepper Stop, LCD Display
+  // 1. POMPA ON
   digitalWrite(POMPA, HIGH);
+  // 2. Servo Kebuka
   servoVertical.write(120);
   servoHorizontal.write(65);
   delay(500);
-
+  // 3. Stepper Stop
   digitalWrite(LPWM_VERTICAL, LOW);
   digitalWrite(RPWM_VERTICAL, LOW);
   analogWrite(PWM_VERTICAL, 0);
   digitalWrite(LPWM_HORIZONTAL, LOW);
   digitalWrite(RPWM_HORIZONTAL, LOW);
   analogWrite(PWM_HORIZONTAL, 0);
-
-  lcd.clear();
+  // 4. LCD Display
   lcd.setCursor(0, 0);
   lcd.print("Fill & Seal Machine");
   lcd.setCursor(0, 1);
@@ -97,65 +56,42 @@ void manual2()
   lcd.setCursor(0, 2);
   lcd.print("T_Horizontal:" + String(temperature_H));
 
+  // ==============================================Kondisi 2===============================================
+  // Kondisi 2: Stepper ON (Tarik Plastik), Servo Ketutup (ngejepit), Pompa ON (Ngisi kemasan), LCD Display
+  // 1. Stepper ON (Tarik Plastik)
   stepsPerRotation += 200;
   stepperVertical.moveTo(stepsPerRotation);
   stepperHorizontal.moveTo(stepsPerRotation);
-
   while (stepperVertical.distanceToGo() != 0 || stepperHorizontal.distanceToGo() != 0) {
     stepperVertical.run();
     stepperHorizontal.run();
-  
   }
-
   digitalWrite(LPWM_VERTICAL, HIGH);
   digitalWrite(RPWM_VERTICAL, LOW);
-  analogWrite(PWM_VERTICAL, 50);
-  // digitalWrite(LPWM_HORIZONTAL, HIGH);
-  // digitalWrite(RPWM_HORIZONTAL, LOW);
-  // analogWrite(PWM_HORIZONTAL, 255);
-
+  analogWrite(PWM_VERTICAL, 50);        // Set PWM (0-255) untuk mengatur besar arus sealer/heater vertical
+  digitalWrite(LPWM_HORIZONTAL, HIGH);
+  digitalWrite(RPWM_HORIZONTAL, LOW);
+  analogWrite(PWM_HORIZONTAL, 255);     // Set PWM (0-255) untuk mengatur besar arus sealer/heater horizontal
+  // 2. Servo ketutup(ngejepit)
   servoVertical.write(20);
   servoHorizontal.write(0);
-  // pompa ON
+  // 3. pompa ON (Ngisi kemasan)
   digitalWrite(POMPA, LOW);
-
-  lcd.clear();
+  // 4. LCD Display
   lcd.setCursor(0, 0);
   lcd.print("Fill & Seal Machine");
   lcd.setCursor(0, 1);
   lcd.print("T_Verical   :" + String(temperature_V));
   lcd.setCursor(0, 2);
   lcd.print("T_Horizontal:" + String(temperature_H));
-  // lcd.setCursor(0, 3);
-  // lcd.print("Pull Plastic");
+  lcd.setCursor(0, 3);
+  count += 1;
+  lcd.print("Jumlah      :" + String(count));
   delay(3000);
+  // ============================================Kembali ke awal===========================================
 }
 
 void loop()
 {
-  // main_heater();
-  manual2();
-  // unsigned long currentTime = millis();
-
-  // read_thermocouple();
-  // main_heater();
-  // moveServo();
-  // testStepper();
-  // moveStepper();
-
-  // if (currentTime - previousTime >= 1000)
-  // {
-  //   Serial.println("Tv:" + String(temperature_V) + " Th:" + String(temperature_H) + "  ||  Heater:" + String(heaterState) + "  Stepper:" + String(isStepperMove) + "  Servo:" + String(isServoMove));
-  // lcd.clear();
-  // lcd.setCursor(0, 0);
-  // lcd.print("Fill & Seal Machine");
-  // lcd.setCursor(0, 1);
-  // lcd.print("T_Verical   :" + String(temperature_V));
-  // lcd.setCursor(0, 2);
-  // lcd.print("T_Horizontal:" + String(temperature_H));
-  // lcd.setCursor(0, 3);
-  // lcd.print("Heat" + String(heaterState) + "  Step" + String(isStepperMove) + "  Servo" + String(isServoMove));
-
-  //   previousTime = currentTime;
-  // }
+  manual_code();
 }
